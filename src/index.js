1b55b71051dac601a353192554be53bd67108cf6
Joey Lundquist <joeylundquist91@gmail.com>
2022-06-18 11:35:46 -0500

    I believe i have what i need installed.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
+import logger from 'redux-logger';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+
+
+
+
+const store = createStore(
+    combineReducers({
+
+    }),applyMiddleware(logger)
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
