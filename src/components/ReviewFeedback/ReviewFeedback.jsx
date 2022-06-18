8041a0dee537ca46b4e10bc59efb7b875c044987
Joey Lundquist <joeylundquist91@gmail.com>
2022-06-18 16:27:29 -0500

    made my server post route tested with postman, now working on client side' :

--- src/components/ReviewFeedback/ReviewFeedback.jsx
+++ src/components/ReviewFeedback/ReviewFeedback.jsx
@@ -1,24 +1,24 @@
 import { useSelector } from "react-redux"
 
 export default function ReviewFeedback() {
-    const feelings = useSelector(store => store.feelingsRating)
+    const feeling = useSelector(store => store.feelingsRating)
     const understanding = useSelector(store => store.understandingRating)
-    const supported = useSelector(store => store.supportedRating)
+    const support = useSelector(store => store.supportedRating)
     const comments = useSelector(store => store.feedbackComments)
 
     const onSubmit = () => {
-        
+
     }
 
     return (
         <>
             <h2>Review your Feedback</h2>
-            <p>Feelings: {feelings}</p>
+            <p>Feelings: {feeling}</p>
             <p>Understanding: {understanding}</p>
-            <p>Supported: {supported}</p>
+            <p>Supported: {support}</p>
             <p>Comments: {comments}</p>
 
-            <button>
+            <button onClick={onSubmit}>
                 Submit
             </button>
         </>
