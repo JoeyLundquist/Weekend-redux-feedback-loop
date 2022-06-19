91ef6d9c82b7a2f9732e8fb40e89f9caf368fa08
Joey Lundquist <joeylundquist91@gmail.com>
2022-06-18 22:07:49 -0500

    base part of admin table started

---
+++ src/components/Comments/Admin/Admin.jsx
@@ -0,0 +1,30 @@
+import './Admin.css'
+
+
+export default function Admin() {
+
+    return ( 
+        <>
+            Testing admin page
+            <table className="admin-table">
+                <thead>
+                    <tr>
+                        <th>
+                            Feeling
+                        </th>
+                        <th>
+                            Understanding
+                        </th>
+                        <th>
+                            Support
+                        </th>
+                        <th>
+                            Comments
+                        </th>
+                        <th></th>
+                    </tr>
+                </thead>
+            </table>
+        </>
+    )
+}
